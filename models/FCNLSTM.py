f2813190ee70ed060c128a3179187cf548f5261a
liadbiz <2712079998@qq.com>
2020-03-06 23:51:40 +0800

    fix typo

--- models/FCNLSTM.py
+++ models/FCNLSTM.py
@@ -1,30 +1,27 @@
-import tensorflow.keras as keras
-from keras.layers import Conv1D, BatchNormalization, GlobalAveragePooling1D, Permute, Dropout, Flatten
-from keras.layers import Input, Dense, LSTM, CuDNNLSTM, concatenate, Activation, GRU, SimpleRNN
-from keras.models import Model
+from tensorflow import keras
 
 def build_lstmfcn(input_shape, num_classes, num_cells):
-    ip = Input(shape=input_shape)
+    ip = keras.layers.Input(shape=input_shape)
 
-    x = LSTM(num_cells)(ip)
-    x = Dropout(0.8)(x)
+    x = keras.layers.LSTM(num_cells)(ip)
+    x = keras.layers.Dropout(0.8)(x)
 
-    y = Permute((2, 1))(ip)
-    y = Conv1D(128, 8, padding='same', kernel_initializer='he_uniform')(y)
-    y = BatchNormalization()(y)
-    y = Activation('relu')(y)
+    y = keras.layers.Permute((2, 1))(ip)
+    y = keras.layers.Conv1D(128, 8, padding='same', kernel_initializer='he_uniform')(y)
+    y = keras.layers.BatchNormalization()(y)
+    y = keras.layers.Activation('relu')(y)
 
-    y = Conv1D(256, 5, padding='same', kernel_initializer='he_uniform')(y)
-    y = BatchNormalization()(y)
-    y = Activation('relu')(y)
+    y = keras.layers.Conv1D(256, 5, padding='same', kernel_initializer='he_uniform')(y)
+    y = keras.layers.BatchNormalization()(y)
+    y = keras.layers.Activation('relu')(y)
 
-    y = Conv1D(128, 3, padding='same', kernel_initializer='he_uniform')(y)
-    y = BatchNormalization()(y)
-    y = Activation('relu')(y)
+    y = keras.layers.Conv1D(128, 3, padding='same', kernel_initializer='he_uniform')(y)
+    y = keras.layers.BatchNormalization()(y)
+    y = keras.layers.Activation('relu')(y)
 
-    y = GlobalAveragePooling1D()(y)
+    y = keras.layers.GlobalAveragePooling1D()(y)
 
-    x = concatenate([x, y])
+    x = keras.layers.concatenate([x, y])
 
-    out = Dense(num_classes, activation='softmax')(x)
+    out = keras.layers.Dense(num_classes, activation='softmax')(x)
     return ip, out

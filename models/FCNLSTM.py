1f9a7e388a517c5b083651a4cd9769006e707dea
liadbiz <2712079998@qq.com>
2020-03-07 00:03:24 +0800

    fix dimension

--- models/FCNLSTM.py
+++ models/FCNLSTM.py
@@ -3,11 +3,12 @@ from tensorflow import keras
 def build_lstmfcn(input_shape, num_classes, num_cells):
     ip = keras.layers.Input(shape=input_shape)
 
-    x = keras.layers.LSTM(num_cells)(ip)
+    x = keras.layers.Permute((2, 1))(ip)
+    x = keras.layers.LSTM(num_cells)(x)
     x = keras.layers.Dropout(0.2)(x)
 
-    y = keras.layers.Permute((2, 1))(ip)
-    y = keras.layers.Conv1D(128, 8, padding='same', kernel_initializer='he_uniform')(y)
+
+    y = keras.layers.Conv1D(128, 8, padding='same', kernel_initializer='he_uniform')(ip)
     y = keras.layers.BatchNormalization()(y)
     y = keras.layers.Activation('relu')(y)
 
